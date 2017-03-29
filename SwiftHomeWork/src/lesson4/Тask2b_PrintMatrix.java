3fc582ef80d4614b0577545d2926220ad7860c16
aizeeck <aizeeck@gmail.com>
2017-03-29 21:43:07 +0300

    Initial commit.

--- SwiftHomeWork/src/lesson4/Тask2b_PrintMatrix.java
+++ SwiftHomeWork/src/lesson4/Тask2b_PrintMatrix.java
@@ -10,23 +10,30 @@ public class Тask2b_PrintMatrix {
         int counter = 0;
         int[][] arr = new int[4][4];
 
-        for (int col = 0; col < arr.length; col++) {
-            for (int row = 0; row < arr[col].length; row++) {
+        for (int col = 0; col < arr.length;) {
+            int row = 0;
+            for (; row < arr[col].length; row++) {
                 arr[row][col] = ++counter;
             }
-//            counter = counter + arr.length;
-//            for (int row = 0; row < arr[col].length; row++) {
-//                arr[col][row] = --counter;
-//            }
-        }
+            //printMe(arr);
 
-        
+            row--;
+            col++;
+            for (; row >= 0; row--) {
+                arr[row][col] = ++counter;
+            }
+            col++;
+            //printMe(arr);
+        }
+        printMe(arr);
+    }
+
+    private static void printMe(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.printf("%4d ", arr[i][j]);
             }
             System.out.println();
         }
-
     }
 }
