0162726018670167aa684b0ef9a120fb253025e1
aizeeck <aizeeck@gmail.com>
2017-04-05 22:17:48 +0300

    aizeeck-ub works correctly

--- SwiftHomeWork/src/lesson4/Task4e_BracketMatching.java
+++ SwiftHomeWork/src/lesson4/Task4e_BracketMatching.java
@@ -21,18 +21,13 @@ public class Task4e_BracketMatching {
         char[] brackets = scanner.nextLine().toCharArray();
         boolean[] substitute = new boolean[brackets.length];
 
-        int open = 0;
-        int close = 0;
-        int currentPosition = 0;
         if (brackets[0] == definitions[1]
                 || brackets[0] == definitions[3]
                 || brackets[0] == definitions[5]) {
-            System.out.println("default: " + false);
             return;
         }
         
         for (int i = 1; i < substitute.length; i++) {
-            currentPosition = i;
             char currentType = 0;
             if (brackets[i] == definitions[1]) {
                 currentType = definitions[0];
@@ -41,11 +36,9 @@ public class Task4e_BracketMatching {
             } else if (brackets[i] == definitions[5]) {
                 currentType = definitions[4];
             }
-            System.out.println("i:" + i + "; brackets[i]" + brackets[i] + "; substitute[i]" + substitute[i]);
-            if (brackets[i] == ')'
-                    || brackets[i] == '}'
-                    || brackets[i] == ']') {
-                System.out.println("    i:" + i);
+            if (brackets[i] == definitions[1]
+                    || brackets[i] == definitions[3]
+                    || brackets[i] == definitions[5]) {
                 substitute[i] = true;
                 for (int j = i - 1; j >= 0; j--) {
                     while (substitute[j] == true) {
@@ -55,8 +48,7 @@ public class Task4e_BracketMatching {
                         substitute[j] = true;
                         break;
                     } else {
-                        System.out.println("i:" + i + "; j:" + j);
-                        System.out.println("deep: " + false);
+                        System.out.println(false);
                         return;
                     }
                 }
