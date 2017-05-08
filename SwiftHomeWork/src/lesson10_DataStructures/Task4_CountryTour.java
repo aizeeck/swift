adb53a6b129c3f4eda46dbc4f7bfe3bcaac7411b
aizeeck <aizeeck@gmail.com>
2017-05-08 23:27:01 +0300

    aizeeck-ub intellij IDEA

--- SwiftHomeWork/src/lesson10_DataStructures/Task4_CountryTour.java
+++ SwiftHomeWork/src/lesson10_DataStructures/Task4_CountryTour.java
@@ -1,7 +1,7 @@
 package lesson10_DataStructures;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -11,12 +11,39 @@ public class Task4_CountryTour {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int citiesCount = scanner.nextInt();
-        Map<Integer, Integer> sities = new HashMap<>();
+        ArrayList<Integer[]> cities = new ArrayList<>();
 
         for (int i = 0; i < citiesCount; i++) {
-            sities.put((Integer) Integer.parseInt(scanner.nextLine()), (Integer) Integer.parseInt(scanner.nextLine()));
+            int s1 = scanner.nextInt();
+            int s2 = scanner.nextInt();
+            cities.add(new Integer[]{s1, s2});
         }
 
-        
+        int count = fuctorial(cities.size());
+        int max = cities.size() - 1;
+        System.out.println("var" + count);
+        int shift = max;
+        Integer[] tmp;
+        while (count > 0) {
+            tmp = cities.get(shift);
+            cities.set(shift, cities.get(shift - 1));
+            cities.set(shift - 1, tmp);
+            print(cities);
+            count--;
+            if (shift < 2) {
+                shift = max;
+            } else {
+                shift--;
+            }
+        }
+    }
+
+    static void print(ArrayList<Integer[]> cities) {
+        cities.forEach(integers -> System.out.print(Arrays.toString(integers)));
+        System.out.println("\n------------------------------------------");
+    }
+
+    static int fuctorial(int n) {
+        return (n > 0) ? n * fuctorial(n - 1) : 1;
     }
 }
